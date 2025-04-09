aae8c60517a3f2af7d466a317deb3f797a22363e
hp <sultonovazizbek73@gmail.com>
2025-04-09 05:13:16 +0500

    async notifier

--- lib/riverpod/riverapi/post_page.dart
+++ lib/riverpod/riverapi/post_page.dart
@@ -3,112 +3,221 @@ import 'package:app1/riverpod/riverapi/post_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+// class PostPage extends ConsumerWidget {
+//   const PostPage({super.key});
+//   void _showForm(BuildContext context, WidgetRef ref,
+//       {bool isEdit = false, PostModel? post}) {
+//     final titleController =
+//         TextEditingController(text: isEdit ? post?.title : '');
+//     final bodyController =
+//         TextEditingController(text: isEdit ? post?.body : '');
+//     showDialog(
+//         context: context,
+//         builder: (context) {
+//           return AlertDialog(
+//             title: Text(isEdit ? "Edit Post" : "Add Post"),
+//             content: Column(
+//               mainAxisSize: MainAxisSize.min,
+//               children: [
+//                 TextField(
+//                   controller: titleController,
+//                   decoration: InputDecoration(
+//                     hintText: 'Title',
+//                   ),
+//                 ),
+//                 TextField(
+//                   controller: bodyController,
+//                   decoration: InputDecoration(
+//                     hintText: 'Body',
+//                   ),
+//                 ),
+//               ],
+//             ),
+//             actions: [
+//               TextButton(
+//                 onPressed: () => Navigator.pop(context),
+//                 child: Text('Cancel'),
+//               ),
+//               TextButton(
+//                 onPressed: () {
+//                   final newPost = PostModel(
+//                       id: isEdit ? post!.id : 0,
+//                       title: titleController.text,
+//                       body: bodyController.text);
+//                   final notifier = ref.read(postProvider.notifier);
+//                   if (isEdit) {
+//                     notifier.editPost(newPost);
+//                   } else {
+//                     notifier.addPost(newPost);
+//                   }
+//                   Navigator.pop(context);
+//                 },
+//                 child: Text(isEdit ? 'Edit' : "Add"),
+//               ),
+//             ],
+//           );
+//         });
+//   }
+//   @override
+//   Widget build(BuildContext context, WidgetRef ref) {
+//     final posts = ref.watch(postProvider);
+//     final postNotifier = ref.read(postProvider.notifier);
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Posts"),
+//       ),
+//       body: posts.isEmpty
+//           ? Center(
+//               child: CircularProgressIndicator(),
+//             )
+//           : ListView.builder(itemBuilder: (context, index) {
+//               return Container(
+//                 padding: EdgeInsets.all(10),
+//                 margin: EdgeInsets.only(top: 5,right: 10,left: 10),
+//                 decoration: BoxDecoration(
+//                   color: Colors.grey[400],
+//                   borderRadius: BorderRadius.circular(15)
+//                 ),
+//                 child: ListTile(
+//                   title: Text(posts[index].title),
+//                   subtitle: Text(posts[index].body),
+//                   trailing: Row(
+//                     mainAxisSize: MainAxisSize.min,
+//                     children: [
+//                       IconButton(
+//                         icon: Icon(Icons.remove),
+//                         onPressed: () => postNotifier.removePost(posts[index].id),
+//                       ),
+//                       IconButton(
+//                           icon: Icon(Icons.edit),
+//                           onPressed: () => _showForm(
+//                                 context,
+//                                 ref,
+//                                 isEdit: true,
+//                                 post: posts[index],
+//                               )),
+//                     ],
+//                   ),
+//                 ),
+//               );
+//             }),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () {
+//           _showForm(context, ref);
+//         },
+//         child: Icon(Icons.add),
+//       ),
+//     );
+//   }
+// }
+
 class PostPage extends ConsumerWidget {
   const PostPage({super.key});
 
-  void _showForm(BuildContext context, WidgetRef ref,
-      {bool isEdit = false, PostModel? post}) {
-    final titleController =
-        TextEditingController(text: isEdit ? post?.title : '');
-    final bodyController =
-        TextEditingController(text: isEdit ? post?.body : '');
+  void _showForm(
+    BuildContext context,
+    WidgetRef ref, {
+    bool isEdit = false,
+    PostModel? post,
+  }) {
+    final titleController = TextEditingController(text: post!.title);
+    final bodyController = TextEditingController(text: post.body);
 
     showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: Text(isEdit ? "Edit Post" : "Add Post"),
-            content: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                TextField(
-                  controller: titleController,
-                  decoration: InputDecoration(
-                    hintText: 'Title',
-                  ),
-                ),
-                TextField(
-                  controller: bodyController,
-                  decoration: InputDecoration(
-                    hintText: 'Body',
-                  ),
-                ),
-              ],
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text(isEdit ? "Edit Post" : "Add Post"),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: titleController,
+              decoration: InputDecoration(hintText: "Title"),
             ),
-            actions: [
-              TextButton(
-                onPressed: () => Navigator.pop(context),
-                child: Text('Cancel'),
-              ),
-              TextButton(
-                onPressed: () {
-                  final newPost = PostModel(
-                      id: isEdit ? post!.id : 0,
-                      title: titleController.text,
-                      body: bodyController.text);
-                  final notifier = ref.read(postProvider.notifier);
-                  if (isEdit) {
-                    notifier.editPost(newPost);
-                  } else {
-                    notifier.addPost(newPost);
-                  }
-                  Navigator.pop(context);
-                },
-                child: Text(isEdit ? 'Edit' : "Add"),
-              ),
-            ],
-          );
-        });
+            TextField(
+              controller: bodyController,
+              decoration: InputDecoration(hintText: "Body"),
+            ),
+          ],
+        ),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: Text('Cancel'),
+          ),
+          TextButton(
+            onPressed: () {
+              final newPost = PostModel(
+                  id: isEdit ? post.id : 0,
+                  title: titleController.text,
+                  body: bodyController.text);
+              final notifier = ref.read(postProvider.notifier);
+              isEdit ? notifier.editPost(newPost) : notifier.addPost(newPost);
+
+              Navigator.pop(context);
+            },
+            child: Text(isEdit ? 'Edit' : "Add"),
+          ),
+        ],
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final posts = ref.watch(postProvider);
-    final postNotifier = ref.read(postProvider.notifier);
-
+    final postsState = ref.watch(postProvider);
+    final notifier = ref.read(postProvider.notifier);
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Posts"),
+      appBar: AppBar(title: Text('Datas'), actions: [
+        IconButton(
+          onPressed: () => notifier.refreshPosts(),
+          icon: Icon(Icons.refresh),
+        ),
+      ]),
+      body: postsState.when(
+        data: (posts) => RefreshIndicator(
+          onRefresh: () => notifier.refreshPosts(),
+          child: ListView.builder(itemBuilder: (context, index) {
+            final post = posts[index];
+            return Container(
+              margin: EdgeInsets.only(top: 5, right: 10, left: 10),
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Colors.grey[400],
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: ListTile(
+                title: Text(post.title),
+                subtitle: Text(post.body),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.edit),
+                      onPressed: () {
+                        notifier.editPost(post);
+                        _showForm(context, ref, isEdit: true, post: post);
+                      },
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.delete),
+                      onPressed: () => notifier.deletePost(post.id),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          }),
+        ),
+        error: (error, st) => Center(
+          child: Text("Error:$error,StatckTrace:$st"),
+        ),
+        loading: () => Center(
+          child: CircularProgressIndicator(),
+        ),
       ),
-      body: posts.isEmpty
-          ? Center(
-              child: CircularProgressIndicator(),
-            )
-          : ListView.builder(itemBuilder: (context, index) {
-              return Container(
-                padding: EdgeInsets.all(10),
-                margin: EdgeInsets.only(top: 5,right: 10,left: 10),
-                decoration: BoxDecoration(
-                  color: Colors.grey[400],
-                  borderRadius: BorderRadius.circular(15)
-                ),
-                child: ListTile(
-                  title: Text(posts[index].title),
-                  subtitle: Text(posts[index].body),
-                  trailing: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      IconButton(
-                        icon: Icon(Icons.remove),
-                        onPressed: () => postNotifier.removePost(posts[index].id),
-                      ),
-                      IconButton(
-                          icon: Icon(Icons.edit),
-                          onPressed: () => _showForm(
-                                context,
-                                ref,
-                                isEdit: true,
-                                post: posts[index],
-                              )),
-                    ],
-                  ),
-                ),
-              );
-            }),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          _showForm(context, ref);
-        },
+        onPressed: () => _showForm(context, ref),
         child: Icon(Icons.add),
       ),
     );
--- lib/riverpod/riverapi/post_provider.dart
+++ lib/riverpod/riverapi/post_provider.dart
@@ -2,38 +2,66 @@ import 'package:app1/riverpod/riverapi/post_model.dart';
 import 'package:app1/riverpod/riverapi/post_service.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class PostNotifier extends StateNotifier<List<PostModel>>{
+// class PostNotifier extends StateNotifier<List<PostModel>>{
+// final PostService postService;
+// PostNotifier(this.postService):super([]){
+//   loadPosts();
+// }
+// Future<void> loadPosts()async{
+//   state=await postService.getPosts();
+// }
+// Future<void> addPost(PostModel post)async{
+//   final newPost=await postService.createPost(post);
+//   state=[newPost,...state,];
+// }
+// Future<void> editPost(PostModel post)async{
+//   final updatePost=await postService.updatePost(post);
+//   state=state.map((e)=>e.id==post.id?updatePost:e).toList();
+// }
+// Future<void> removePost(int id)async{
+//   await postService.deletePost(id);
+//   state=state.where((e)=>e.id!=id).toList();
+// }
+// }
+// final postServiceProvider=Provider((ref)=>PostService());
+// final postProvider=StateNotifierProvider<PostNotifier,List<PostModel>>((ref){
+//   return PostNotifier(ref.watch(postServiceProvider),);
+// });
 
-final PostService postService;
-PostNotifier(this.postService):super([]){
-  loadPosts();
+final postServiceProvider = Provider((ref) => PostService());
+
+final postProvider =
+    AsyncNotifierProvider<PostNotifier, List<PostModel>>(PostNotifier.new);
+
+class PostNotifier extends AsyncNotifier<List<PostModel>> {
+  late final PostService _service;
+
+  Future<List<PostModel>> build() async {
+    _service = ref.read(postServiceProvider);
+    return await _service.getPosts();
+  }
+
+  Future<void> addPost(PostModel post) async {
+    final newPost = await _service.createPost(post);
+    state = AsyncData([newPost, ...state.value!]);
+  }
+
+  Future<void> editPost(PostModel post) async {
+    final updated = await _service.updatePost(post);
+    state = AsyncData(
+      state.value!.map((e) => e.id == updated.id ? updated : e).toList(),
+    );
+  }
+
+  Future<void> deletePost(int id) async {
+    await _service.deletePost(id);
+    state = AsyncData(
+      state.value!.where((e) => e.id != id).toList(),
+    );
+  }
+
+  Future<void> refreshPosts() async {
+    state = const AsyncLoading();
+    state = AsyncData(await _service.getPosts());
+  }
 }
-
-
-Future<void> loadPosts()async{
-  state=await postService.getPosts();
-}
-
-Future<void> addPost(PostModel post)async{
-  final newPost=await postService.createPost(post);
-  state=[newPost,...state,];
-}
-
-Future<void> editPost(PostModel post)async{
-  final updatePost=await postService.updatePost(post);
-  state=state.map((e)=>e.id==post.id?updatePost:e).toList();
-}
-
-Future<void> removePost(int id)async{
-  await postService.deletePost(id);
-  state=state.where((e)=>e.id!=id).toList();
-}
-
-}
-
-
-final postServiceProvider=Provider((ref)=>PostService());
-
-final postProvider=StateNotifierProvider<PostNotifier,List<PostModel>>((ref){
-  return PostNotifier(ref.watch(postServiceProvider),);
-});
